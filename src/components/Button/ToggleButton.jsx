10af82b7ec6afb778b22278e1cdaba8235766e00
celine <wisohyun0@gmail.com>
2025-08-12 00:10:24 +0900

    chore: toggle button 주석 작성 및 필요없는 코드삭제

--- src/components/Button/ToggleButton.jsx
+++ src/components/Button/ToggleButton.jsx
@@ -1,10 +1,33 @@
 import { cn } from "../../utils";
-
+/**
+ * 토글 버튼 그룹을 감싸는 래퍼 컴포넌트
+ *
+ * @author <sohyun>
+ * @component
+ * @param {Object} props - 컴포넌트 props
+ * @param {React.ReactNode} props.children - 자식 컴포넌트들
+ * @returns {JSX.Element} 토글 버튼 그룹 래퍼
+ */
 export const ToggleButtonWrapper = ({ children }) => {
   return (
     <div className={cn("w-[244px] flex flex-row bg-gray-100")}>{children}</div>
   );
 };
+
+/**
+ * 활성/비활성 상태를 지원하는 토글 버튼 컴포넌트
+ *
+ * @author <sohyun>
+ * @component
+ * @param {Object} props - 컴포넌트 props
+ * @param {boolean} [props.isActive=false] - 활성화 여부
+ * @param {function} [props.onClick] - 클릭 이벤트 핸들러
+ * @param {React.ReactNode} props.children - 버튼 내부에 렌더링할 요소
+ * @returns {JSX.Element} 토글 버튼 요소
+ *
+ * @example
+ * <ToggleButton isActive={true} onClick={handleClick}>컬러</ToggleButton>
+ */
 const ToggleButton = ({ isActive = false, onClick, children, ...props }) => {
   const activeClass = isActive
     ? "bg-white shadow-purple-600-2 text-purple-700 font-bold hover:bg-purple-100 active:bg-purple-200"
@@ -27,10 +50,4 @@ const ToggleButton = ({ isActive = false, onClick, children, ...props }) => {
 
 export default ToggleButton;
 
-// Usage example
-// <ToggleButton isActive={true} onClick={() => console.log('Clicked!')}>
-//   Toggle Me
-// </ToggleButton>
-// <ToggleButton isActive={false} onClick={() => console.log('Clicked!')}>
-//   Toggle Me
-// </ToggleButton>
+
