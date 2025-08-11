e3336efc5a332eccce0148b23daf3a82dc7d08cd
JungHoon <fbwjdgns05@naver.com>
2025-08-11 19:45:43 +0900

    rename: 디렉토리 경로 설정

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./main.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CardListPage from "./pages/CardListPage.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ src/pages/CardListPage.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { Link } from "react-router-dom";
+//import CardList from '../features/CardList/CardList';
+//import Header from '헤더경로';
