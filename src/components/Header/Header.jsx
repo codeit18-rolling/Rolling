98affa716ac2e52483f79779df45d71a94fee354
Imhwitae <ghkdgnlxo2@gmail.com>
2025-08-12 11:04:20 +0900

    fix: 모바일 해상도에서 메인 페이지나 리스트 페이지가 아닐 때 헤더 컴포넌트를 안보이게 수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,64 +1,78 @@
 import { useLocation } from "react-router";
 import icRolling from "../../assets/icon/ic-rolling-paperplain.svg";
 import { cn } from "../../utils";
+import useMediaQuery from "../../features/header/hooks/useMediaQuery";
 
 /**
  * 헤더 컴포넌트
  * @author <hwitae>
  */
 export const Header = () => {
+  /**
+   * 페이지의 뷰포트가 모바일인지 확인
+   */
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  /**
+   * 현재 페이지 경로
+   */
   const { pathname } = useLocation();
 
   return (
     <>
-      <header className={cn("hidden", "tablet:flex", "desktop:flex")}>
-        <div className={cn("border-b border-gray-headerBorder")}>
-          <div
-            className={cn(
-              "w-[360px]",
-              "h-[64px]",
-              "flex",
-              "items-center",
-              "justify-between",
-              "m-auto",
-              "tablet:w-[768px]",
-              "desktop:w-[1248px]",
-              "px-6"
-            )}
-          >
-            <div className={cn("flex", "gap-x-2")}>
-              <img
-                className={cn("w-[27px]", "h-[27px]")}
-                src={icRolling}
-                alt="로고이미지"
-              />
-              <p
-                className={cn(
-                  "font-[poppins]",
-                  "text-20",
-                  "font-semibold",
-                  "text-gray-headerLogo"
-                )}
-              >
-                Rolling
-              </p>
-            </div>
-            <div className={cn("text-center")}>
-              {pathname === "/" || pathname === "/list" ? (
-                <button
+      <header>
+        {isMobile && pathname !== "/" && pathname !== "/list" ? null : (
+          <div className={cn("border-b border-gray-headerBorder")}>
+            <div
+              className={cn(
+                "mobile:w-[360px]",
+                "h-[64px]",
+                "flex",
+                "items-center",
+                "justify-between",
+                "m-auto",
+                "tablet:w-[768px]",
+                "desktop:w-[1248px]",
+                "px-6"
+              )}
+            >
+              <div className={cn("flex", "gap-x-2")}>
+                <img
+                  className={cn("w-[27px]", "h-[27px]")}
+                  src={icRolling}
+                  alt="로고이미지"
+                />
+                <p
                   className={cn(
-                    "border-solid",
-                    "border-black",
-                    "border",
-                    "w-[157px]"
+                    "font-[poppins]",
+                    "text-20",
+                    "font-semibold",
+                    "text-gray-headerLogo"
                   )}
                 >
-                  버튼 들어갈 자리
-                </button>
-              ) : null}
+                  Rolling
+                </p>
+              </div>
+              <div className={cn("text-center")}>
+                {pathname === "/" || pathname === "/list" ? (
+                  <button
+                    className={cn(
+                      "border-solid",
+                      "border-black",
+                      "border",
+                      "w-[157px]"
+                    )}
+                  >
+                    버튼 들어갈 자리
+                  </button>
+                ) : null}
+              </div>
             </div>
           </div>
-        </div>
+        )}
+        {pathname !== "/" && pathname !== "/list" ? (
+          <div>헤더 서비스 들어갈 자리</div>
+        ) : null}
       </header>
     </>
   );
