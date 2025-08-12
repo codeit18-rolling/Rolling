f4cab6f15cecb0cdbedc18d7e25e48865c8e6070
JungHoon <fbwjdgns05@naver.com>
2025-08-12 22:36:32 +0900

    feat: 카드리스트 컴포넌트의 컨테이너 부분 스타일링

--- src/features/CardList/CardList.jsx
+++ src/features/CardList/CardList.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
+const BG_COLOR = {
+  purple: "bg-purple-200",
+  blue: "bg-blue-200",
+  green: "bg-green-200",
+  beige: "bg-beige-200",
+};
+
 const CardList = ({ item }) => {
   return (
-    <div className="w-[275px] h-[260px] pt-[30px] pr-[24px] pb-[20px] pl-[24px] rounded-[16px] border border-black/10">
+    <div
+      className={`w-[275px] h-[260px] pt-[30px] pb-[20px] px-[24px] rounded-[16px] border border-black/10 flex relative justify-start items-start flex-col ${
+        !item.backgroundImageURL ? BG_COLOR[item.backgroundColor] : ""
+      }`}
+    >
       {/*container*/}
       <div className="">
         {/*to. ~ , 몇명이 작성 등등이 들어갈 곳 */}
-        <div className="text-2xl">
+        <div className="text-24">
           {/*받는사람 */}
           <span
             className={`font-bold text-2xl ${
@@ -18,7 +29,7 @@ const CardList = ({ item }) => {
         </div>
         <div className="">{/*프로필 이미지들*/}</div>
         <div
-          className={` text-[16px] ${
+          className={` text-16 ${
             item.backgroundURL ? "text-gray-200" : "text-gray-700"
           }`}
         >
@@ -27,7 +38,7 @@ const CardList = ({ item }) => {
           <span className="font-[400]">명이 작성했어요!</span>
         </div>
       </div>
-      <div className="">
+      <div className="top-28">
         {/* emoji 들어갈 곳 */}
         {item.topReactions.map((reaction) => (
           <div key={reaction.id} className="">
@@ -39,3 +50,5 @@ const CardList = ({ item }) => {
     </div>
   );
 };
+
+export default CardList;
