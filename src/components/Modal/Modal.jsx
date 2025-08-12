06cc581e2eaab7bac17753bb671ea99288223a3f
JinHyuk <dog33027@gmail.com>
2025-08-12 18:03:05 +0900

    design: Modal 컴포넌트에 Tailwind 스타일 적용

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -20,26 +20,39 @@ const Modal = ({
     : "";
 
   return createPortal(
-    <div onMouseDown={onBackdropMouseDown}>
-      <div role="dialog" aria-modal="true">
-        <div>
-          <div>
-            <img src={profileImageURL} alt={`${sender} 프로필`} />
-            <div>
-              <div>
+    <div
+      // onMouseDown={onBackdropMouseDown}
+      className="fixed inset-0 z-99 flex bg-black/40 items-center justify-center"
+    >
+      <div
+        role="dialog"
+        aria-modal="true"
+        className="rounded-[20px] bg-white shadow-lg border border-gray-200 p-6 w-[600px] h-[476px] max-w-[90vw] max-h-[90vh]"
+      >
+        <div className="flex items-start justify-between">
+          <div className="flex items-center gap-3">
+            <img
+              src={profileImageURL}
+              alt={`${sender} 프로필`}
+              className="size-10 rounded-full object-cover"
+            />
+            <div className="flex flex-col">
+              <div className="">
                 From. <span>{sender}</span>
               </div>
-              <span>{relationship}</span>
+              <span className="inline-flex w-fit px-2 py-0.5 mt-1 rounded-md">
+                {relationship}
+              </span>
             </div>
           </div>
           <span>{formattedDate}</span>
         </div>
 
-        <div />
+        <div className="my-4 h-px bg-gray-200" />
 
-        <div>{content}</div>
+        <div className="h-[260px] overflow-y-auto pr-1">{content}</div>
 
-        <div>
+        <div className="mt-6 flex justify-center">
           <button type="button" onClick={onConfirm ?? onClose}>
             확인
           </button>
