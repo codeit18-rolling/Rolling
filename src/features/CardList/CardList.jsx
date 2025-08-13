8bf2666034fb71039492f101c708eb395bf69ee3
JungHoon <fbwjdgns05@naver.com>
2025-08-13 12:17:25 +0900

    asset: background pattern 1234 svg 파일 추가

--- src/features/CardList/CardList.jsx
+++ src/features/CardList/CardList.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const BG_COLOR = {
   purple: "bg-purple-200",
+  beige: "bg-beige-200",
   blue: "bg-blue-200",
   green: "bg-green-200",
-  beige: "bg-beige-200",
+};
+const BG_PATTERN = {
+  purple: "",
+  beige: "",
+  blue: "",
+  green: "",
 };
 
 const CardList = ({ item }) => {
