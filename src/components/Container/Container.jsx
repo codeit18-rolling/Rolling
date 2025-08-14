c8c639add19b33f46ee7815a76314926fc5acd53
celine <wisohyun0@gmail.com>
2025-08-14 19:44:01 +0900

    docs: 공통 컨테이너 주석추가

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,21 @@
 import { cn } from "../../utils";
-
+/**
+ *  레이아웃을 위한 컨테이너 컴포넌트
+ *
+ * @author <sohyun>
+ * @component
+ * @param {Object} props - 컴포넌트 props
+ * @param {React.ReactNode} props.children - 컨테이너 내부에 렌더링할 요소
+ * @param {string} [props.className] - 컨테이너에 적용할 추가 CSS 클래스명
+ * @param {boolean} [props.isInnerBox=false] - 내부 박스 사용 여부
+ * @param {string} [props.innerBoxClassName] - 내부 박스에 적용할 추가 CSS 클래스명
+ * @returns {JSX.Element} 컨테이너 요소
+ *
+ * @example
+ * <Container>/Container> -- 기본 
+ * <Container isInnerBox ></Container> -- To / from
+ * <Container isInnerBox innerBoxClassName="bg-white"></Container> --  To / from 커스텀 
+ */
 const Container = ({
   children,
   className = "",
