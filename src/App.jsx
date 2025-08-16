7d51d29c9a255d07dc9b34e21a1919c7d0132d10
wlrnjs <tjwlrnjs7336@icloud.com>
2025-08-16 13:56:08 +0900

    chore: App.jsx import 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,4 @@
-import { cn } from "./utils";
 import { Header } from "./components/Header/Header";
-import { REACTIONS_DATA, MESSAGES_DATA, BACKGROUND_IMG_DATA } from "./MockData";
-import DropdownEmoji from "./components/Dropdown/DropdownEmoji";
-
-import Input from "./components/Input/Input";
-import { DropdownAddEmoji } from "./components/Dropdown/DropdownAddEmoji";
-import Writers from "./components/Writers/Writers";
-import Option from "./features/Option/Option";
-import SkeletonUI from "./components/Skeleton/SkeletonUI";
-import { ShareButton } from "./features/HeaderService/HeaderServiceElements/ShareButton";
-import CardListSet from "./features/CardList/CardListSet";
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import Lists from "./pages/Lists";
