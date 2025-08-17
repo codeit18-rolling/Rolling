8b45f87757424173bbe5f0e86f4a08c351e1ff33
Imhwitae <ghkdgnlxo2@gmail.com>
2025-08-17 22:34:22 +0900

    design: 메인 페이지 디자인 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import Container from "../components/Container/Container";
+import imgMainMobile from "../assets/img/img_main_01_2x.png";
+import { cn } from "../utils";
 
 /**
  * 메인 페이지 컴포넌트
@@ -7,7 +9,39 @@ import Container from "../components/Container/Container";
 function Home() {
   return (
     <>
-      <Container></Container>
+      <Container>
+        <div
+          className={cn(
+            "w-[320px] h-[352px] rounded-[20px] bg-surface overflow-x-hidden"
+          )}
+        >
+          <div className="pt-6 px-6">
+            <div
+              className={cn(
+                "bg-purple-600 w-[80px] px-3 py-1 rounded-[50px]",
+                " text-[14px] font-bold text-white leading-[20px] -tracking-[0.005em]"
+              )}
+            >
+              Point. 01
+            </div>
+            <div className="mt-4">
+              <p className="text-18 font-bold leading-[28px] -tracking-[0.01em]">
+                누구나 손쉽게, 온라인 롤링 페이퍼를 만들 수 있어요
+              </p>
+              <p className="text-[15px] font-normal leading-[22px] -tracking-[-0.01em] text-gray-500">
+                로그인 없이 자유롭게 만들어요
+              </p>
+            </div>
+            <div className="flex mt-[50px] justify-center">
+              <img
+                src={imgMainMobile}
+                alt="메인이미지1"
+                className="max-w-none w-[400px] object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
