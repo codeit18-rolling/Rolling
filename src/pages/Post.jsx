cfb955a565fe466bacc24b0ab5f07ca29222a6c5
celine <wisohyun0@gmail.com>
2025-08-17 15:44:39 +0900

    feat: post 페이지 api 작업

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
-import { BACKGROUND_IMG_DATA } from "../MockData";
+import { useNavigate } from "react-router";
 import PostInput from "./PostElements/PostInput";
 import PostOption from "./PostElements/PostOption";
 import Container from "../components/Container/Container";
@@ -7,7 +7,8 @@ import Button from "../components/Button/Button";
 import FloatingButton from "../components/Button/FloatingButton";
 import useInputValidator from "../hooks/useInputValidator";
 import { validateName } from "../utils/validate";
-
+import getBackgroundImage from "../service/getBackgroundImages";
+import createRecipient from "../service/createRecipient";
 const DEFAULT_COLOR = "beige";
 const Post = () => {
   const [createPostData, setCreatePostData] = useState({
@@ -16,17 +17,11 @@ const Post = () => {
     backgroundColor: DEFAULT_COLOR,
     backgroundImageURL: null,
   });
+  const [images, setImages] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const { errorMsg, handleChange, handleBlur, handleValidateSubmit } =
     useInputValidator(createPostData.name, validateName);
-
-  const handleInput = (e) => {
-    setCreatePostData((prev) => ({
-      ...prev,
-      name: e.target.value,
-    }));
-    handleChange(e);
-  };
+  const navigate = useNavigate();
 
   const handleColorSelect = useCallback((color) => {
     setCreatePostData((prev) => ({
@@ -35,23 +30,49 @@ const Post = () => {
       backgroundImageURL: null, // 컬러 선택 시 이미지 제거
     }));
   }, []);
-
   const handleImageSelect = useCallback((url) => {
     setCreatePostData((prev) => ({
       ...prev,
       backgroundImageURL: url, // 컬러는 필수 값이므로 이미지만 변경
     }));
   }, []);
-
-  const handleSubmit = () => {
-    if (!handleValidateSubmit()) return;
-    console.log("서버 전송 데이터:", createPostData);
+  const handleInput = (e) => {
+    setCreatePostData((prev) => ({
+      ...prev,
+      name: e.target.value,
+    }));
+    handleChange(e);
   };
-  // 이미지 로딩 시뮬레이션 (API에서 받는다고 가정)
-  useEffect(() => {
-    setTimeout(() => {
+  const handleImageLoad = async () => {
+    try {
+      setIsLoading(true);
+      const image = await getBackgroundImage();
+      setImages(image);
+    } catch (error) {
+      console.log("이미지를 불러오는데 실패했습니다:", error);
+    } finally {
       setIsLoading(false);
-    }, 1500);
+    }
+  };
+  const handleSubmit = async () => {
+    if (!handleValidateSubmit()) return;
+    try {
+      const result = await createRecipient(createPostData);
+      navigate(`/post/${result.id}`);
+      console.log("Post 요청 성공:", result);
+    } catch (error) {
+      console.error("Post 요청 실패:", error);
+    } finally {
+      // 성공/실패 상관없이 입력값 초기화
+      setCreatePostData((prev) => ({
+        ...prev,
+        name: "",
+      }));
+    }
+  };
+
+  useEffect(() => {
+    handleImageLoad();
   }, []);
   return (
     <Container isInnerBox={true} innerBoxClassName="flex flex-col gap-[50px]">
@@ -62,13 +83,13 @@ const Post = () => {
         onBlur={handleBlur}
       />
       <PostOption
-        bgImages={BACKGROUND_IMG_DATA}
+        bgImages={images}
         isLoading={isLoading}
         onColorSelect={handleColorSelect}
         onImageSelect={handleImageSelect}
       ></PostOption>
       <FloatingButton>
-        <Button className="w-full" onClick={handleSubmit}>
+        <Button className="w-full" onClick={handleSubmit} disabled={errorMsg}>
           생성하기
         </Button>
       </FloatingButton>
