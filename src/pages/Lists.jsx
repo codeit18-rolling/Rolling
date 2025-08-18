3ebdf9d7d2cbc3e67c54bc4b009e496604b8f314
JungHoon <fbwjdgns05@naver.com>
2025-08-18 20:32:46 +0900

    refactor: Lists 페이지 안 상수 컴포넌트에서 분리

--- src/pages/Lists.jsx
+++ src/pages/Lists.jsx
@@ -2,24 +2,24 @@ import CardListSet from "../features/CardList/CardListSet";
 import Button from "../components/Button/Button";
 import { Link } from "react-router";
 
-function Lists() {
-  const DESIGN = {
-    List: "flex flex-col gap-3 mobile:gap-4 mt-[40px] mobile:mt-[50px]",
-    Msg: "text-20 mobile:text-24 font-bold text-black ml-5 mobile:ml-6 tablet:ml-5",
-  };
-  const ListPageMap = [
-    {
-      title: "인기 롤링 페이퍼 🔥",
-      sortOrder: "reactionCount",
-    },
-    {
-      title: "최근에 만든 롤링 페이퍼 ⭐️️",
-      sortOrder: "createdAt",
-    },
-  ];
+const DESIGN = {
+  List: "flex flex-col gap-3 mobile:gap-4 mt-[40px] mobile:mt-[50px]",
+  Msg: "text-20 mobile:text-24 font-bold text-black ml-5 mobile:ml-6 tablet:ml-5",
+};
+const ListPageMap = [
+  {
+    title: "인기 롤링 페이퍼 🔥",
+    sortOrder: "reactionCount",
+  },
+  {
+    title: "최근에 만든 롤링 페이퍼 ⭐️️",
+    sortOrder: "createdAt",
+  },
+];
 
+function Lists() {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col tablet:items-center">
       <div className="flex flex-col tablet:w-[1200px] justify-start tablet:items-center">
         {ListPageMap.map(({ title, sortOrder }) => (
           <div className={`${DESIGN.List}`} key={title}>
