bc5e1ca6d478f45992bdfa1811bcc303bd5b3abf
JungHoon <fbwjdgns05@naver.com>
2025-08-19 18:57:39 +0900

    style: 주석 변경

--- src/features/CardList/CardListSet.jsx
+++ src/features/CardList/CardListSet.jsx
@@ -77,7 +77,7 @@ const CardListSet = ({ sortOrder }) => {
       </div>
 
       <div className="flex flex-row gap-3 overflow-x-auto scrollbar-hide tablet:hidden mobile:gap-5 pl-5 mobile:pl-6 ">
-        {/*모바일/태블릿용 CardList를 새로 뽑아봅시다.*/}
+        {/*모바일/태블릿용 CardList*/}
         {items.map((item) => (
           <CardList key={item.id} item={item} />
         ))}
