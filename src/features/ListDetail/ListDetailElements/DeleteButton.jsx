69e26e020280e910c6aefc6debe50ebe6d58f243
wlrnjs <tjwlrnjs7336@icloud.com>
2025-08-20 01:21:43 +0900

    feat: 삭제하기 모달 연결

--- src/features/ListDetail/ListDetailElements/DeleteButton.jsx
+++ src/features/ListDetail/ListDetailElements/DeleteButton.jsx
@@ -2,9 +2,12 @@ import Button from "../../../components/Button/Button";
 import { useNavigate } from "react-router";
 import deleteRecipient from "../../../service/ListDetails/deleteRecipientsDetail";
 import { cn } from "../../../utils";
+import { useState } from "react";
+import DeleteModal from "../../../components/Modal/DeleteModal";
 
 const DeleteButton = ({ id }) => {
   const navigate = useNavigate();
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   // 카드 전체 삭제 함수
   const deleteButtonHandler = async () => {
@@ -20,7 +23,7 @@ const DeleteButton = ({ id }) => {
       )}
     >
       <Button
-        onClick={deleteButtonHandler}
+        onClick={() => setIsDeleteModalOpen(true)}
         btnStyle="primary"
         className={cn(
           "flex justify-end w-[calc(100vw-130px)] h-[44px] rounded-[8px] font-normal text-[16px] leading-[26px] tracking-[-0.01em] text-white bg-purple-600",
@@ -30,6 +33,14 @@ const DeleteButton = ({ id }) => {
       >
         삭제하기
       </Button>
+
+      {isDeleteModalOpen && (
+        <DeleteModal
+          isOpen={isDeleteModalOpen}
+          onClose={() => setIsDeleteModalOpen(false)}
+          onConfirm={deleteButtonHandler}
+        />
+      )}
     </div>
   );
 };
