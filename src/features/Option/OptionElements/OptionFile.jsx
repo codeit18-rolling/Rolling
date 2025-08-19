7dc538e4788e6da322896f757af4c41438f3463a
celine <wisohyun0@gmail.com>
2025-08-20 00:57:33 +0900

    feat: 이미지 로딩 스피너 추가 및 컴포넌트 분리

--- src/features/Option/OptionElements/OptionFile.jsx
+++ src/features/Option/OptionElements/OptionFile.jsx
@@ -1,77 +1,67 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import Icon from "../../../components/Icon/Icon";
 import { cn } from "../../../utils";
 import { validateFile } from "../../../utils/validate";
 import useCloudinaryUpload from "../../Post/hooks/useCloudinaryUpload";
+import OptionFileLoadContent from "./OptionFileLoadContent";
+const labelClasses = [
+  "group w-full cursor-pointer p-6 mt-10",
+  "flex flex-col items-center justify-center gap-2 rounded-2xl relative",
+  "border-[3px] border-dotted border-gray-300",
+  "after:w-full after:h-full after:relative after:z-[1]",
+  "focus:shadow-purple-500 focus:border-purple-500 hover:border-purple-500",
+];
+const iconClasses = "bg-gray-400 group-hover:bg-purple-500";
+const LIMIT_SIZE = 15;
 
-const LIMIT_SIZE = 5;
 const OptionFile = ({ onUpload }) => {
   const { uploadFiles, isUploading } = useCloudinaryUpload();
   const [isActive, setIsActive] = useState(false);
   const handleDragStart = () => setIsActive(true);
   const handleDragEnd = () => setIsActive(false);
   const handleDragOver = (e) => e.preventDefault(); // 파일 드롭시 새창 이슈
-
-  const getFileInfo = async (files) => {
-    const validFiles = validateFile(files, LIMIT_SIZE);
-    const uploadedUrls = await uploadFiles(validFiles); // 멀티업로드 후 URL 배열 반환
-
-    onUpload(uploadedUrls);
+  const handleDrag = {
+    start: () => setIsActive(true),
+    end: () => setIsActive(false),
+    over: (e) => e.preventDefault(), // 파일 드롭시 새창 이슈
+    drop: (e) => {
+      e.preventDefault();
+      setIsActive(false);
+      handleFiles(Array.from(e.dataTransfer.files));
+    },
   };
+  const handleFiles = useCallback(
+    async (files) => {
+      const validFiles = validateFile(files, LIMIT_SIZE);
+      if (validFiles.length === 0) return;
 
-  const handleDrop = (e) => {
-    e.preventDefault();
-    setIsActive(false);
-    const files = Array.from(e.dataTransfer.files);
-    getFileInfo(files);
-  };
-
-  const handleFile = (e) => {
-    const files = Array.from(e.target.files);
-    getFileInfo(files);
-  };
+      const uploadedUrls = await uploadFiles(validFiles); // 멀티업로드 후 URL 배열 반환
+      onUpload(uploadedUrls);
+    },
+    [uploadFiles, onUpload]
+  );
 
   return (
     <label
       htmlFor="imageUpload"
       aria-label="이미지 업로드"
-      className={cn(
-        "group w-full cursor-pointer p-6 mt-10",
-        "flex flex-col items-center justify-center gap-2 rounded-2xl relative",
-        "border-[3px] border-dotted border-gray-300",
-        "after:w-full after:h-full after:relative after:z-[1]",
-        "focus:shadow-purple-500 focus:border-purple-500 hover:border-purple-500",
-        isActive && "border-purple-500"
-      )}
-      onDragOver={handleDragOver}
-      onDragEnter={handleDragStart}
-      onDragLeave={handleDragEnd}
-      onDrop={handleDrop}
+      className={cn(labelClasses, isActive && "border-purple-500")}
+      onDragOver={handleDrag.over}
+      onDragEnter={handleDrag.start}
+      onDragLeave={handleDrag.end}
+      onDrop={handleDrag.drop}
     >
-      <Icon
-        iconName="plus"
-        className={cn(
-          "bg-gray-400 group-hover:bg-purple-500",
-          isActive && "bg-purple-500"
-        )}
-      ></Icon>
-      <p
-        className={cn(
-          "text-14 text-center break-keep text-gray-400 font-semibold",
-          "group-hover:text-purple-500",
-          isActive && "text-purple-500"
-        )}
-      >
-        {isUploading
-          ? "업로드 중..."
-          : `${LIMIT_SIZE}MB 이하만 업로드 가능합니다`}
-      </p>
+      <OptionFileLoadContent
+        isUploading={isUploading}
+        isActive={isActive}
+        size={LIMIT_SIZE}
+      />
       <input
         type="file"
-        name="imageUpload"
         id="imageUpload"
+        name="imageUpload"
         accept="image/*"
-        onChange={handleFile}
+        onChange={(e) => handleFiles(Array.from(e.target.files))}
         className="hidden"
       />
     </label>
---
+++ src/features/Option/OptionElements/OptionFileLoadContent.jsx
@@ -0,0 +1,31 @@
+import Icon from "../../../components/Icon/Icon";
+import { cn } from "../../../utils";
+
+const iconStyle = "bg-gray-400 group-hover:bg-purple-500";
+const textStyle =
+  "text-14 text-center break-keep text-gray-400 font-semibold group-hover:text-purple-500";
+const loadingSpinner =
+  "inline-block w-2 h-2 ml-6 rounded-full bg-purple-500 shadow-loading-spinner animate-loading-spinner";
+
+const OptionFileLoadContent = ({ isUploading, isActive, size }) => {
+  return (
+    <>
+      {isUploading ? (
+        <div className="text-purple-500 ">
+          업로드 중 <span className={loadingSpinner}></span>
+        </div>
+      ) : (
+        <>
+          <Icon
+            iconName="plus"
+            className={cn(iconStyle, isActive && "bg-purple-500")}
+          ></Icon>
+          <div className={cn(textStyle, isActive && "text-purple-500")}>
+            {`${size}MB 이하만 업로드 가능합니다`}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+export default OptionFileLoadContent;
