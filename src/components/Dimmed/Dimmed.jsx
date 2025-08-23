f8844b8e6786e1b9e4111e51bcae57edf286637a
wlrnjs <tjwlrnjs7336@icloud.com>
2025-08-23 14:05:53 +0900

    fix: 리뷰 수정

--- src/components/Dimmed/Dimmed.jsx
+++ src/components/Dimmed/Dimmed.jsx
@@ -2,7 +2,7 @@ const Dimmed = () => {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none absolute inset-0 black-opacity-5"
+      className="pointer-events-none absolute inset-0 bg-black-opacity-5"
     />
   );
 };
