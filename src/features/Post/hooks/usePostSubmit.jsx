1669827b6941b6739480e0e2688f2a42c9eb0ae2
JungHoon <fbwjdgns05@naver.com>
2025-08-25 13:21:28 +0900

    feat: CardList 최신화를 위한 invalidate 추가

--- src/features/Post/hooks/usePostSubmit.jsx
+++ src/features/Post/hooks/usePostSubmit.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router";
 import createRecipient from "../../../service/Post/createRecipient";
+import { useQueryClient } from "@tanstack/react-query";
 
 const usePostSubmit = (createPostData, resetName) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const handleSubmit = async (validateFn) => {
     if (!validateFn()) return;
@@ -13,6 +15,9 @@ const usePostSubmit = (createPostData, resetName) => {
       console.error("Post 요청 실패:", error);
     } finally {
       // 성공/실패 상관없이 입력값 초기화
+      queryClient.invalidateQueries({
+        queryKey: ["recipients"],
+      });
       resetName();
     }
   };
